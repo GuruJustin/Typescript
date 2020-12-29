b3259c67b667d314684bf8abb7f13b52898141ee
gurujustin <meanjustin2020@hotmail.com>
2020-12-29 10:46:56 -0500

    babel polyfill setup:

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,20 +2,28 @@ const path = require("path")
 
 module.exports = {
   entry: {
-    main: "./src/main.js"
+    main: ["core-js/fn/promise","./src/main.js"]
   },
   mode: "development",
   output: {
     filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/"
+    path: path.resolve(__dirname, "../dist")
   },
-  devServer: {
+  devServer: {  
     contentBase: "dist",
     overlay: true
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
       {
         test: /\.css$/,
         use: [{ loader: "style-loader" }, { loader: "css-loader" }]
@@ -41,24 +49,13 @@ module.exports = {
             }
           },
           {
-            loader: "extract-loader"
+            loader: "extract-loader",
+            options: {
+              publicPath: "../"
+            }
           },
           {
-            loader: "html-loader",
-            options : {
-              attrs: ["img:src"]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(jpg|gif|png)$/,
-        use : [
-          {
-            loader : 'file-loader',
-            options : {
-              name : "img/[name].[ext]"
-            }
+            loader: "html-loader"
           }
         ]
       }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
+require("babel-runtime/regenerator")
 require("./main.css")
 require("./index.html")
+
+var a = async ( args ) => {
+    const {a,b} = args
+    await console.log("hello form the future", a,b)
+    console.log("Done")
+}
+
+a({a:23,b:23})
